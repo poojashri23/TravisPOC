30433bd15939dde6389ed10da26ed84eb8d2d0c8
Macbook <macbook@192.168.1.3>
2020-10-29 16:27:28 +0530

    Editing the README to try out git add/commit

---
+++ TravisPOC/TravisPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravisPOC
+//
+//  Created by Macbook on 29/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
